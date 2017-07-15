6f12735a92d7cf680dcd587b886df1267fa99e3a
KwabenaOduro <oduro.kwabena.patrick@gmail.com>
2017-07-15 10:56:40 +0000

    live chat

---
+++ www/js/services/socket.js
@@ -0,0 +1,45 @@
+
+// angular socket wrapper for socket.io
+
+angular.module('VideoChatApp.services')
+	.factory('Socket', function(socketFactory, $timeout) {
+
+		// connect to our server
+		//var socket = io.connect('http://192.168.0.214:9000/');
+        //http://127.0.0.1:8080
+        //var socket = io.connect('http://127.0.0.1:9000/');
+        //var socket = io();  
+        var socket = io.connect('https://mlivechat.herokuapp.com/');
+		var socketFactory = socketFactory({
+			ioSocket: socket
+		});
+
+		// generate a unique custom request id
+		var makeId = function(len) {
+			var text = '';
+			var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$';
+			for (var i = 0; i < (len || 10); i++) {
+				text += possible.charAt(Math.floor(Math.random() * possible.length));
+			}
+			return text;
+		}
+
+		// send an event and get a response back
+		socketFactory.promise = function(eventName, request) {
+			return new Promise(function(resolve, reject) {
+
+				var success = function(response) {
+					console.debug(eventName + '|' + request.responseName + ': complete!');
+					socketFactory.removeListener(request.responseName, success);
+					resolve(response);
+				};
+
+				request.responseName = '$response$' + makeId() + '$';
+				console.debug(eventName + '|' + request.responseName + ': Sending socket promise...');
+				socketFactory.on(request.responseName, success);
+				socketFactory.emit(eventName, request);
+			});
+		};
+
+		return socketFactory;
+	})
